4645d52eb32fe0681f88bcfde498abc8f1374a45
Ivan Marosevic <ivanmarosevic96@gmail.com>
2019-08-20 17:32:15 +0200

    making firebase authentication

---
+++ Pods/Target Support Files/Pods-ShootingApp/Pods-ShootingApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShootingApp : NSObject
+@end
+@implementation PodsDummy_Pods_ShootingApp
+@end
---
+++ ShootingApp/StartViewController.swift
@@ -0,0 +1,48 @@
+//
+//  StartViewController.swift
+//  ShootingApp
+//
+//  Created by Ivan Marosevic on 20/08/2019.
+//  Copyright © 2019 Marosevic d.o.o. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    // MARK: - Outlets
+
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        nameLabel.text = "ShootingApp"
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func signUpTouched(_ sender: Any) {
+    }
+    
+    
+    @IBAction func loginTouched(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
