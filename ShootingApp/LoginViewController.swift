38b356386e6fab07d56d0d10f7b62642ce7eb361
Ivan Marosevic <ivanmarosevic96@gmail.com>
2019-08-22 08:47:49 +0200

    login and sign up finsehd

--- ShootingApp/LoginViewController.swift
+++ ShootingApp/LoginViewController.swift
@@ -7,24 +7,63 @@
 //
 
 import UIKit
+import Firebase
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: - Outlets
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var emailLoginText: UITextField!
+    @IBOutlet weak var passwordLoginText: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        emailLabel.text = "E-mail"
+        passwordLabel.text = "Password"
+        
+        emailLoginText.delegate = self
+        passwordLoginText.delegate = self
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    @IBAction func loginButtonTouched(_ sender: Any) {
+        
+        Auth.auth().signIn(withEmail: emailLoginText.text!, password: passwordLoginText.text!) { (user, error) in
+            
+            if error == nil {
+                self.performSegue(withIdentifier: "loginToHome", sender: self)
+                print("Congrats")
+            } else {
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+        
     }
-    */
+    
+
+    // Functions or methods
+    func hidekeyboard() {
+        emailLoginText.resignFirstResponder()
+        passwordLoginText.resignFirstResponder()
+        
+    }
+    
+    
+    // UITextFieldDelegate Methods
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        hidekeyboard()
+        return true
+    }
 
 }
--- ShootingApp/SignUpViewController.swift
+++ ShootingApp/SignUpViewController.swift
@@ -8,24 +8,80 @@
 //
 
 import UIKit
+import Firebase
 
-class SignUpViewController: UIViewController {
-
+class SignUpViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var emailSignUpLabel: UILabel!
+    @IBOutlet weak var passwordSignUpLabel: UILabel!
+    @IBOutlet weak var rePassSignUpLabel: UILabel!
+    @IBOutlet weak var emailSignUpTextField: UITextField!
+    @IBOutlet weak var passwordSignUpTextField: UITextField!
+    @IBOutlet weak var rePasswordSignUpTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        emailSignUpLabel.text = "E-mail"
+        passwordSignUpLabel.text = "Password"
+        rePassSignUpLabel.text = "Retype password"
+        
+        emailSignUpTextField.delegate = self
+        passwordSignUpTextField.delegate = self
+        rePasswordSignUpTextField.delegate = self
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    @IBAction func signUpTouched(_ sender: Any) {
+        if passwordSignUpLabel.text != rePassSignUpLabel.text {
+            
+            let alertController = UIAlertController(title: "Password incorrect", message: "Please retype your password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        } else {
+            
+            Auth.auth().createUser(withEmail: emailSignUpTextField.text!, password: passwordSignUpTextField.text!) { (user, error) in
+                
+                if error == nil {
+                    
+                    self.performSegue(withIdentifier: "signUpToHome", sender: self)
+                    print("Succesfull signup")
+                } else {
+                    
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+                
+            
+            }
+        }
     }
-    */
+    
+    // Functions or methods
+    func hidekeyboard() {
+        emailSignUpTextField.resignFirstResponder()
+        passwordSignUpTextField.resignFirstResponder()
+        rePasswordSignUpTextField.resignFirstResponder()
+        
+    }
+    
+
+    // UITextFieldDelegate Methods
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        hidekeyboard()
+        return true
+    }
 
 }
