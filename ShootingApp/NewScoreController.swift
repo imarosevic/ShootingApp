cf56da52eeabba91cfd73e8b31da9b8c0e0b7224
Ivan Marosevic <ivanmarosevic96@gmail.com>
2019-09-12 15:02:37 +0200

    added imageview

--- ShootingApp/NewScoreController.swift
+++ ShootingApp/NewScoreController.swift
@@ -10,7 +10,7 @@ import UIKit
 import FirebaseDatabase
 import Firebase
 
-class NewScoreController: UIViewController, UITextFieldDelegate {
+class NewScoreController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var round1: UITextField!
     @IBOutlet weak var round2: UITextField!
@@ -18,17 +18,20 @@ class NewScoreController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var round4: UITextField!
     @IBOutlet weak var round5: UITextField!
     @IBOutlet weak var round6: UITextField!
-    @IBOutlet weak var sumResult: UILabel!
-    @IBOutlet weak var imagePicker: UIImageView!
+    @IBOutlet weak var addImage: UIButton!
+    @IBOutlet weak var selectedImage: UIImageView!
     
     @IBOutlet weak var add: UIButton!
 
     
     var ref: DatabaseReference?
+    let imagePicker =  UIImagePickerController()
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
 //        round1.keyboardType = .numberPad
+        
         
         round1.delegate = self
         round2.delegate = self
@@ -38,6 +41,8 @@ class NewScoreController: UIViewController, UITextFieldDelegate {
         round6.delegate = self
         
         ref = Database.database().reference(fromURL: "https://ioshootingapp.firebaseio.com/leaderboards")
+        
+      
         
     }
 
@@ -51,19 +56,19 @@ class NewScoreController: UIViewController, UITextFieldDelegate {
         round6.resignFirstResponder()
     }
     
-//    func calculate(_ sender: AnyObject) -> String{
-//        let round1Int: Int? = Int(round1.text!)
-//        let round2Int: Int? = Int(round2.text!)
-//        let round3Int: Int? = Int(round3.text!)
-//        let round4Int: Int? = Int(round4.text!)
-//        let round5Int: Int? = Int(round5.text!)
-//        let round6Int: Int? = Int(round6.text!)
-//
-//        let convert = round1Int + round2Int + round3Int + round4Int + round5Int + round6Int
-//        let convertText = String(convert)
-//
-//        return convert
-//    }
+    func calculate() -> String{
+        let round1Int = Int(round1.text!) ?? 0
+        let round2Int = Int(round2.text!) ?? 0
+        let round3Int = Int(round3.text!) ?? 0
+        let round4Int = Int(round4.text!) ?? 0
+        let round5Int = Int(round5.text!) ?? 0
+        let round6Int = Int(round6.text!) ?? 0
+
+        let convert = round1Int + round2Int + round3Int + round4Int + round5Int + round6Int
+        let convertText = String(convert)
+
+        return convertText
+    }
     
     // UITextFieldDelegate Methods
     
@@ -71,20 +76,73 @@ class NewScoreController: UIViewController, UITextFieldDelegate {
         hidekeyboard()
         return true
     }
+    @IBAction func addImageTouched(_ sender: Any) {
+        
+    
+        let alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
+            self.openCamera()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { _ in
+            self.openGallary()
+        }))
+        
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+        
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func openCamera()
+    {
+         imagePicker.delegate = self
+
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera))
+        {
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        else
+        {
+            let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func openGallary()
+    {
+        imagePicker.delegate = self
+        
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        imagePicker.allowsEditing = true
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+
+        if let image = info[UIImagePickerController] as? UIImage {
+            selectedImage.image = image
+    }
+    }
     
      @IBAction func addTouched(_ sender: Any) {
+        
+        let sum = calculate()
        
         if round1.text?.isEmpty ?? true || round2.text?.isEmpty ?? true || round3.text?.isEmpty ?? true || round4.text?.isEmpty ?? true || round5.text?.isEmpty ?? true || round6.text?.isEmpty ?? true {
             print("error")
         } else {
-            self.ref?.child("leaderboards").setValue(["round1": round1.text!,
+            self.ref?.childByAutoId().setValue(["round1": round1.text!,
                                                   "round2": round2.text!,
                                                   "round3": round3.text!,
                                                   "round4": round4.text!,
                                                   "round5": round5.text!,
                                                   "round6": round6.text!,
-                                                  "sum": sumResult.text!,
-                                                  "timestamp": Date()])
+                                                  "sum": sum])
             
             self.performSegue(withIdentifier: "newToScoreboard", sender: self)
 //        sumResult.text = Int(round1.text!)
@@ -92,3 +150,4 @@ class NewScoreController: UIViewController, UITextFieldDelegate {
 
     }
 }
+
